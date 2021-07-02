74afc7aba298f5ef2b563ae5787156c89998ec7a
powskierman <mlapoint99@hotmail.com>
2021-07-02 11:46:36 -0400

    Initial commit

---
+++ src/ds18b20.cpp
@@ -0,0 +1,10 @@
+//Initialize the sensor. Use pin 0. Sensor type is DHT22.
+#include "settings.h"
+#include "dhtModule.h"
+
+DHT dht(0,DHT22); 
+
+void startDht(){
+  dht.begin(); //Start temperature sensor
+  delay(1500);
+}
